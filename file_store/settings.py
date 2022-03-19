51916e366e5e6378b1830a70c214cfcc2acee7eb
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2022-03-19 19:46:04 +0500

    secret removed

--- file_store/settings.py
+++ file_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^&f@ene(*-y4v&(3c%rhbt%ed5656r&+x(40m7420glw0nni$@'
+SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
