eedc73fc20f3237d5caf0451cada98fcf2e04211
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2022-03-21 15:19:08 +0500

    turned off DEBUG

--- file_store/settings.py
+++ file_store/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','spekit-challenge-api.herokuapp.com']
